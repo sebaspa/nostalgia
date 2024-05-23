c13fef58c67cf6757240ea32df5984a7d5a447ac
Camilo Pinzon <pinzonac@gmail.com>
2024-05-23 12:03:31 -0500

    products by category widget

--- plugins/nostalgia/widgets/index.php
+++ plugins/nostalgia/widgets/index.php
@@ -8,6 +8,7 @@ function nostalgia_widgets()
   require_once dirname(__FILE__) . '/contactMap.php';
   require_once dirname(__FILE__) . '/serviceOptions.php';
   require_once dirname(__FILE__) . '/featuredProducts.php';
+  require_once dirname(__FILE__) . '/producstByCategory.php';
 }
 
 add_action('widgets_init', 'nostalgia_widgets');
